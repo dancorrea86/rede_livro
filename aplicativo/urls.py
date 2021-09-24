5164fa9249a67b9cec301d085274b857f0423cb2
dancorrea86 <danielmacielcorrea@gmail.com>
2021-09-24 14:53:22 -0300

    html files working

---
+++ aplicativo/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register', views.register, name='register'),
+    path('users', views.users, name='users'),
+    path('books', views.books, name='books'),
+    path('users/<int:user_id>', views.user, name='user'),
+    path('books/<int:book_id>', views.book, name='book'),
+]
--- aplicativo/views.py
+++ aplicativo/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+def home(request):
+    return render(request,'pages/base.html')
+
+def books(request):
+    return render(request, "pages/books.html")
+
+def users(request):
+    return render(request, "pages/users.html")
+
+def book(request, book_id):
+    return render(request, "pages/book_profile.html")
+
+def user(request, user_id):
+    return render(request, "pages/user_profile.html")
+
+def register(request):
+    return render(request, "pages/register.html")
+    
